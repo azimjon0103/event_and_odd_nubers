172fc7aa6f328af776cbb2bdaee65ac2319fd7c6
azimjon0103 <azimjonfayzullayev3@gmail.com>
2022-10-07 13:18:24 +0500

    04

--- number_odd.py
+++ number_odd.py
@@ -1,13 +1,16 @@
 #A four-digit integer is given. Find the number of odd digits in it.
-a=3756
+from re import A
+
+
+var_int=3756
 #Create a variable "var_int" and assign it a four-digit integer value.
-b=a//1000%2
-d=a-b*1000
+b=var_int//1000%2
+d=var_int-b*1000
 c=d//100%2
 e=d-c*100
 f=e//10%2
 g=e-f*10 
 h=g%2
 #Print the number of odd digits in the variable "var_int".
-var_int=b+c+f+h
-print(var_int)
+a=b+c+f+h
+print(a)
